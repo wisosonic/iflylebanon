2502d32e1ccae48c2619495e20f25f248f05049f
rayane <rayannajjar26@gmail.com>
2018-06-14 02:07:08 +0300

    new place

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php namespace App\Http\Controllers;
 
 use Auth;
+use App\Place;
 
 use Illuminate\Support\Facades\Request;
 
@@ -8,7 +9,8 @@ class HomeController extends Controller {
 
 	public function homepage()
 	{
-		return view("homepage");
+		$places = Place::all();
+		return view("homepage", ["places"=>$places]);
 	}
 
 }
--- app/Place.php
+++ app/Place.php
@@ -1,17 +1,19 @@
 <?php namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use App\User;
 
 class Place extends Model  {
 	protected $table = "places";
 	protected $fillable = [
-							"tite",
+							"title",
 							"department",
 							"coordinates",
 							"description",
 							"rating",
 							"user_id"
-							];
+						];
 
 
 	public function user()
@@ -22,6 +24,22 @@ class Place extends Model  {
 	public function ratings()
 	{
 		return $this->hasMany("App\Rating");
+	}
+
+	public function placeInsert(Request $request)
+	{
+		$user = Auth::user(); 
+
+		$data = $request->all;
+		$newPlace = new Place();
+		$newPlace->title=$data['title'];
+		$newPlace->department=$data['department'];
+		$newPlace->title=$data['coordinates'];
+		$newPlace->description=$data['description'];
+		$newPlace->rating=$data['rating'];
+		$newPlace->user_id=$user->id;
+		$newPlace->save();
+		
 	}
 
 }
