78fcbe5a1785fb3631117bb780e02218f5a09e0c
Wassim <wiso_1988@hotmail.com>
2018-06-28 16:09:20 +0200

    youtube api

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'login',
-        '/logout'
+        '/logout',
+        '/oauth2callback'
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,18 @@
 */
 
 Route::get("/test1", function(){
+	$params=array('eventType' => 'live', 'maxResults' => 25, 'q' => 'afqa', 'type' => 'video', 'part'=>"snippet");
 
 	$youtube = new Madcoda\Youtube\Youtube(array('key' => 'AIzaSyAZMuhMd1c1awtDuM0rhwkdbzOLyomRlgc'));
 	// $search = $youtube->getChannelById('UCQedLwrgw<eJ-GgGzH6JywQg');
-	$search = $youtube->searchChannelVideos('afqa', 'UCQedLwrgweJ-GgGzH6JywQg', 50);
+	$search = $youtube->searchAdvanced($params, true);
 	
 	dd($search);
 
+});
+
+Route::get("/test2", function(){
+	return view("test2");
 });
 
 Route::get('/', "HomeController@homepage");
