96f0c6406d44aecd02422fe4d2d1b1ee5b157b24
rayane <rayannajjar26@gmail.com>
2018-07-06 00:47:59 +0300

    lklk
    lkkm

--- resources/views/Places/addnewplace.blade.php
+++ resources/views/Places/addnewplace.blade.php
@@ -8,6 +8,10 @@
     .checked {
         color: orange;
     }
+    input[type="text"],
+    textarea {
+      width: 100%
+    }
   </style>
 
   <div id="sitecontainer">
@@ -42,7 +46,21 @@
             </div>
             <div class="col-md-12 full-width">
               <label for="description">Description : </label>
-              <input type="text" name="description" id="description" placeholder="Description">
+              <textarea rows="2" name="description" id="description" placeholder="Description"></textarea>
+            </div>
+            <div class="col-md-12 full-width">
+              <label for="text">About this place : </label>
+              <textarea rows="5" name="text" id="text" placeholder="About this place"></textarea>
+            </div>
+            <div class="col-md-12 full-width">
+              <label>Locate this place on the map : </label>
+              <div id="latclicked"></div>
+              <div id="longclicked"></div>
+              <div id="latmoved"></div>
+              <div id="longmoved"></div>
+              <div style="padding:10px">
+                  <div id="map"></div>
+              </div>
             </div>
          </div>
       </div>
@@ -50,5 +68,74 @@
 
   </div>
 
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZMuhMd1c1awtDuM0rhwkdbzOLyomRlgc&callback=initMap" async defer></script>
+  <script type="text/javascript">
+    var map;
+
+    function initMap() {                            
+    var latitude = 27.7172453; // YOUR LATITUDE VALUE
+    var longitude = 85.3239605; // YOUR LONGITUDE VALUE
+
+    var myLatLng = {lat: latitude, lng: longitude};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: myLatLng,
+      zoom: 14,
+      disableDoubleClickZoom: true, // disable the default map zoom on double click
+    });
+
+    // Update lat/long value of div when anywhere in the map is clicked    
+    google.maps.event.addListener(map,'click',function(event) {                
+        document.getElementById('latclicked').innerHTML = event.latLng.lat();
+        document.getElementById('longclicked').innerHTML =  event.latLng.lng();
+    });
+
+    // Update lat/long value of div when you move the mouse over the map
+    google.maps.event.addListener(map,'mousemove',function(event) {
+        document.getElementById('latmoved').innerHTML = event.latLng.lat();
+        document.getElementById('longmoved').innerHTML = event.latLng.lng();
+    });
+            
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      //title: 'Hello World'
+      
+      // setting latitude & longitude as title of the marker
+      // title is shown when you hover over the marker
+      title: latitude + ', ' + longitude 
+    });    
+
+    // Update lat/long value of div when the marker is clicked
+    marker.addListener('click', function(event) {              
+      document.getElementById('latclicked').innerHTML = event.latLng.lat();
+      document.getElementById('longclicked').innerHTML =  event.latLng.lng();
+    });
+
+    // Create new marker on double click event on the map
+    google.maps.event.addListener(map,'dblclick',function(event) {
+        var marker = new google.maps.Marker({
+          position: event.latLng, 
+          map: map, 
+          title: event.latLng.lat()+', '+event.latLng.lng()
+        });
+        
+        // Update lat/long value of div when the marker is clicked
+        marker.addListener('click', function() {
+          document.getElementById('latclicked').innerHTML = event.latLng.lat();
+          document.getElementById('longclicked').innerHTML =  event.latLng.lng();
+        });            
+    });
+
+    // Create new marker on single click event on the map
+    /*google.maps.event.addListener(map,'click',function(event) {
+        var marker = new google.maps.Marker({
+          position: event.latLng, 
+          map: map, 
+          title: event.latLng.lat()+', '+event.latLng.lng()
+        });                
+    });*/
+    }
+    </script>
 
 @endsection
