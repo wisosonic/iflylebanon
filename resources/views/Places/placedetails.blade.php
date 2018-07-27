09044c92c137439ea1e6e370725111c359b6d197
rayane <rayannajjar26@gmail.com>
2018-07-27 22:52:28 +0300

    place details modifs

--- resources/views/Places/placedetails.blade.php
+++ resources/views/Places/placedetails.blade.php
@@ -301,6 +301,35 @@
                               </div>
                               <div class="clear"></div>
                            </div>
+                           @if (count($related)>0)
+                              <div>
+                                 <h4>Related places</h4>
+                                 <div class="single-slideshow gallery-wrap info pager no-autoplay" style="background: none;">
+                                    <div class="bx-wrapper" style="max-width: 100%;">
+                                       <div class="bx-viewport" style="width: 100%; overflow: hidden; position: relative; height: 523px;">
+                                          <ul id="gallery-2" class="bxslider gallery galleryid-111 gallery-columns-3 gallery-size-fullslideshownc pager nocaption nolink slideshow contentslideshow " style="width: auto; position: absolute; opacity: 1;">
+                                             @foreach ($related as $key => $value)
+                                                <li class="gallery-icon linked" style="float: none; list-style: none; position: absolute; width: 730px; z-index: 50; display: block;"><a href="/location/{{$value->slug}}" class="lightbox-gallery no-ajaxy"><img src="{{$value->image}}" class="attachment-fullslideshownc size-fullslideshownc" alt="" title="chellel afka2_HDR2" srcset="https://parposa.com/iflylebanon/wp-content/uploads/2018/05/chellel-afka2_HDR2-1.jpg 800w, https://parposa.com/iflylebanon/wp-content/uploads/2018/05/chellel-afka2_HDR2-1-300x215.jpg 300w, https://parposa.com/iflylebanon/wp-content/uploads/2018/05/chellel-afka2_HDR2-1-768x550.jpg 768w, https://parposa.com/iflylebanon/wp-content/uploads/2018/05/chellel-afka2_HDR2-1-740x530.jpg 740w, https://parposa.com/iflylebanon/wp-content/uploads/2018/05/chellel-afka2_HDR2-1-540x387.jpg 540w" sizes="(max-width: 800px) 100vw, 800px"></a></li>
+                                             @endforeach
+                                          </ul>
+                                       </div>
+                                       <div class="bx-controls bx-has-controls-direction bx-has-pager" style="opacity: 1;">
+                                          <div class="bx-controls-direction">
+                                             <a class="bx-prev" href="https://parposa.com/iflylebanon/location/afqa-waterfall/">Prev</a>
+                                             <a class="bx-next" href="https://parposa.com/iflylebanon/location/afqa-waterfall/">Next</a>
+                                          </div>
+                                          <div class="bx-pager bx-default-pager">
+                                             <div class="bx-pager-item"><a href="https://parposa.com/iflylebanon/location/afqa-waterfall/" data-slide-index="0" class="bx-pager-link active">1</a></div>
+                                             <div class="bx-pager-item"><a href="https://parposa.com/iflylebanon/location/afqa-waterfall/" data-slide-index="1" class="bx-pager-link">2</a></div>
+                                             <div class="bx-pager-item"><a href="https://parposa.com/iflylebanon/location/afqa-waterfall/" data-slide-index="2" class="bx-pager-link">3</a></div>
+                                             <div class="bx-pager-item"><a href="https://parposa.com/iflylebanon/location/afqa-waterfall/" data-slide-index="3" class="bx-pager-link">4</a></div>
+                                          </div>
+                                       </div>
+                                    </div>
+                                    <div class="clear"></div>
+                                 </div>
+                              </div>
+                           @endif
                            <div class="sharing badges section-buttons v-item">
                               <a href="https://parposa.com/iflylebanon/location/afqa-waterfall/#share-box" onclick="updateShareBoxLinks(this);" data-title="Afqa Waterfall" data-link="https://parposa.com/iflylebanon/location/afqa-waterfall/" class="location-actions open-popup-link post-share share popup-with-move-anim">
                               <i class="fa fa-share"></i>
